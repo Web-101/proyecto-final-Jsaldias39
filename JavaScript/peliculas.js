239d5ca0cf79cebda8465f6bfb2be87c2313b832
Justin David Saldias <136645726+Jsaldias39@users.noreply.github.com>
2026-07-03 19:52:21 -0400

    Actualizacion con JavaScript

---
+++ JavaScript/peliculas.js
@@ -0,0 +1,57 @@
+const peliculas = {
+    thebatman: {
+        titulo: "The Batman",
+        categoria: "Acción",
+        duracion: "176 min",
+        calificacion: "4.9",
+        idioma: "Sub/Esp",
+        sinopsis: "En su segundo año luchando contra el crimen, Batman explora la corrupción en Gotham City y el vínculo de su propia familia, mientras se enfrenta a un asesino en serie conocido como el Acertijo.",
+        imagen: "Recursos/thebatman.jpg",
+        estreno: "ESTRENO",
+        sala: "2D+3D"
+    },
+    transformers: {
+        titulo: "Transformers 3",
+        categoria: "Acción",
+        duracion: "154 min",
+        calificacion: "4.7",
+        idioma: "Sub/Esp",
+        sinopsis: "Los Autobots y los Decepticons se involucran en una peligrosa carrera espacial entre los Estados Unidos y la Unión Soviética, lo que podría desencadenar una guerra que destruya la Tierra.",
+        imagen: "Recursos/transformersdarkofthemoon.jpg",
+        estreno: "ESTRENO",
+        sala: "IMAX"
+    },
+    lanaranjamecanica: {
+        titulo: "La Naranja Mecánica",
+        categoria: "Drama",
+        duracion: "136 min",
+        calificacion: "4.8",
+        idioma: "Sub/Esp",
+        sinopsis: "En una Gran Bretaña futurista, Alex DeLarge, un delincuente obsesionado con la ultraviolencia y Beethoven, es capturado y sometido a un innovador tratamiento de rehabilitación conductual.",
+        imagen: "Recursos/lanaranjamecanica.jpg",
+        estreno: "CLÁSICO",
+        sala: "2D"
+    },
+    ironlung: {
+        titulo: "Iron Lung",
+        categoria: "Terror",
+        duracion: "90 min",
+        calificacion: "4.5",
+        idioma: "Sub/Esp",
+        sinopsis: "Un convicto es enviado a pilotar un submarino monoplaza improvisado a través de un océano de sangre en una luna alienígena desolada, buscando recursos vitales para la supervivencia de la humanidad.",
+        imagen: "Recursos/ironlung.jpg",
+        estreno: "IMAX",
+        sala: "2D"
+    },
+    jackassforever: {
+        titulo: "Jackass Forever",
+        categoria: "Comedia",
+        duracion: "96 min",
+        calificacion: "4.6",
+        idioma: "Sub/Esp",
+        sinopsis: "Celebrando la alegría de estar de vuelta con sus mejores amigos, el equipo original de Jackass se reúne para otra ronda de demostraciones cómicas absurdas, divertidas y a menudo peligrosas.",
+        imagen: "Recursos/jackassforever.jpg",
+        estreno: "NUEVO",
+        sala: "2D"
+    }
+};
